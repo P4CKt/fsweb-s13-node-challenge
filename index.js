a248f436957e35cdd3841a63e65bd910ec4a125e
P4CKt <omurcanuslu@outlook.com>
2023-03-24 20:21:35 +0300

    installation done

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+const port = process.env.PORT || 9001;
+const server = require("./api/server");
+
+server.listen(port, () => console.log(`Server is listening on ${port}`));
